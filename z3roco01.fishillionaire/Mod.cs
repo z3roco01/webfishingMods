39c60628f680c7a0680000ea4a48cbbe51d2f858
zerocool <46464404+z3roco01@users.noreply.github.com>
2024-10-31 20:56:16 -0500

    upload fishillionaire ( forgot oopsies )

---
+++ z3roco01.fishillionaire/Mod.cs
@@ -0,0 +1,19 @@
+﻿using GDWeave;
+using GDWeave.Godot;
+using GDWeave.Godot.Variants;
+using GDWeave.Modding;
+
+namespace z3roco01.fishillionaire;
+
+public class Mod : IMod {
+    public static IModInterface modInterface;
+
+    public Mod(IModInterface ModInterface) {
+        modInterface = ModInterface;
+        ModInterface.RegisterScriptMod(new ScratchSpotsMod());
+        modInterface.Logger.Information("YOURE THE FISHILLIONAIRE !!!!");
+    }
+
+    public void Dispose() {
+    }
+}
---
+++ z3roco01.fishillionaire/ScratchSpotsMod.cs
@@ -0,0 +1,40 @@
+﻿using GDWeave;
+using GDWeave.Godot;
+using GDWeave.Godot.Variants;
+using GDWeave.Modding;
+using System.Net.Security;
+
+namespace z3roco01.fishillionaire;
+
+public class ScratchSpotsMod : IScriptMod {
+    // only runs on the player gd script
+    public bool ShouldRun(string path) => path == "res://Scenes/Minigames/ScratchTicket/scratch_spots.gdc";
+
+    public IEnumerable<Token> Modify(string path, IEnumerable<Token> tokens) {
+        // wait for chance = 0.7
+        var assignWaiter = new MultiTokenWaiter([
+            t => t is IdentifierToken{Name:"chance"},
+            t => t.Type is TokenType.OpAssign,
+            t => t is ConstantToken{Value:RealVariant{Value:0.7}}
+        ], allowPartialMatch: false);
+
+        // wait for chance *= 0.75
+        var mulAssignWaiter = new MultiTokenWaiter([
+            t => t is IdentifierToken{Name:"chance"},
+            t => t.Type is TokenType.OpAssignMul,
+            t => t is ConstantToken{Value:RealVariant{Value:0.75}}
+        ], allowPartialMatch: false);
+
+        foreach (var token in tokens) {
+            if (assignWaiter.Check(token)) {
+                yield return new ConstantToken(new RealVariant(1.0));
+            }
+            else if (mulAssignWaiter.Check(token)) {
+                yield return new ConstantToken(new RealVariant(1.0));
+            }
+            else {
+                yield return token;
+            }
+        }
+    }
+}
